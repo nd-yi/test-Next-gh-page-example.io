7838d67b9daaebc4ced78cc0a152f9baa54538cd
nd-yi <songyi@nd.com.cn>
2022-01-21 18:24:58 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/qJK5IP8hh7QO3AILAvd5d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c83d8b52a6a77b24.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],"/about":["static/chunks/pages/about-54dcf363a797bc94.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
